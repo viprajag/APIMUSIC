9d6db28206ca653c957074caf3644758d166e8e7
viprajag <viprajatg@gmail.com>
2025-12-11 12:56:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # Git
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itzshukla/APIMUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/viprajag/APIMUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
